39ee8b452c5d0da78128b7a277ad8b5505aa2764
healthywalk <89001945+healthywalk@users.noreply.github.com>
2021-08-22 08:17:40 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -124,31 +124,31 @@ namespace VL53L1X {
             readReg(PAD_I2C_HV__EXTSUP_CONFIG) | 0x01)
         fast_osc_frequency = readReg16Bit(OSC_MEASURED__FAST_OSC__FREQUENCY)
         osc_calibrate_val = readReg16Bit(RESULT__OSC_CALIBRATE_VAL)
-        writeReg16Bit(DSS_CONFIG__TARGET_TOTAL_RATE_MCPS, TargetRate) // should already be this value after reset
+        writeReg16Bit(DSS_CONFIG__TARGET_TOTAL_RATE_MCPS, TargetRate)
         writeReg(GPIO__TIO_HV_STATUS, 0x02)
-        writeReg(SIGMA_ESTIMATOR__EFFECTIVE_PULSE_WIDTH_NS, 8) // tuning parm default
-        writeReg(SIGMA_ESTIMATOR__EFFECTIVE_AMBIENT_WIDTH_NS, 16) // tuning parm default
+        writeReg(SIGMA_ESTIMATOR__EFFECTIVE_PULSE_WIDTH_NS, 8)
+        writeReg(SIGMA_ESTIMATOR__EFFECTIVE_AMBIENT_WIDTH_NS, 16)
         writeReg(ALGO__CROSSTALK_COMPENSATION_VALID_HEIGHT_MM, 0x01)
         writeReg(ALGO__RANGE_IGNORE_VALID_HEIGHT_MM, 0xFF)
-        writeReg(ALGO__RANGE_MIN_CLIP, 0) // tuning parm default
-        writeReg(ALGO__CONSISTENCY_CHECK__TOLERANCE, 2) // tuning parm default
+        writeReg(ALGO__RANGE_MIN_CLIP, 0)
+        writeReg(ALGO__CONSISTENCY_CHECK__TOLERANCE, 2)
 
         writeReg16Bit(SYSTEM__THRESH_RATE_HIGH, 0x0000)
         writeReg16Bit(SYSTEM__THRESH_RATE_LOW, 0x0000)
         writeReg(DSS_CONFIG__APERTURE_ATTENUATION, 0x38)
 
-        writeReg16Bit(RANGE_CONFIG__SIGMA_THRESH, 360) // tuning parm default
-        writeReg16Bit(RANGE_CONFIG__MIN_COUNT_RATE_RTN_LIMIT_MCPS, 192) // tuning parm default
+        writeReg16Bit(RANGE_CONFIG__SIGMA_THRESH, 360)
+        writeReg16Bit(RANGE_CONFIG__MIN_COUNT_RATE_RTN_LIMIT_MCPS, 192)
 
         writeReg(SYSTEM__GROUPED_PARAMETER_HOLD_0, 0x01)
         writeReg(SYSTEM__GROUPED_PARAMETER_HOLD_1, 0x01)
-        writeReg(SD_CONFIG__QUANTIFIER, 2) // tuning parm default
+        writeReg(SD_CONFIG__QUANTIFIER, 2)
 
         writeReg(SYSTEM__GROUPED_PARAMETER_HOLD, 0x00)
-        writeReg(SYSTEM__SEED_CONFIG, 1) // tuning parm default
-        writeReg(SYSTEM__SEQUENCE_CONFIG, 0x8B) // VHV, PHASECAL, DSS1, RANGE
-        writeReg16Bit(DSS_CONFIG__MANUAL_EFFECTIVE_SPADS_SELECT, 200 << 8);
-        writeReg(DSS_CONFIG__ROI_MODE_CONTROL, 2) // REQUESTED_EFFFECTIVE_SPADS
+        writeReg(SYSTEM__SEED_CONFIG, 1)
+        writeReg(SYSTEM__SEQUENCE_CONFIG, 0x8B)
+        writeReg16Bit(DSS_CONFIG__MANUAL_EFFECTIVE_SPADS_SELECT, 200 << 8)
+        writeReg(DSS_CONFIG__ROI_MODE_CONTROL, 2)
         setLongDistanceMode()
         setMeasurementTimingBudget(50000)
         writeReg16Bit(ALGO__PART_TO_PART_RANGE_OFFSET_MM,
@@ -161,22 +161,22 @@ namespace VL53L1X {
         writeReg(RANGE_CONFIG__VALID_PHASE_HIGH, 0xB8)
         writeReg(SD_CONFIG__WOI_SD0, 0x0F)
         writeReg(SD_CONFIG__WOI_SD1, 0x0D)
-        writeReg(SD_CONFIG__INITIAL_PHASE_SD0, 14) // tuning parm default
-        writeReg(SD_CONFIG__INITIAL_PHASE_SD1, 14) // tuning parm default
+        writeReg(SD_CONFIG__INITIAL_PHASE_SD0, 14)
+        writeReg(SD_CONFIG__INITIAL_PHASE_SD1, 14)
     }
 
     function setMeasurementTimingBudget(budget_us: number): boolean {
         if (budget_us <= TimingGuard) { return false }
         budget_us -= TimingGuard
         let range_config_timeout_us = budget_us
-        if (range_config_timeout_us > 1100000) { return false } // FDA_MAX_TIMING_BUDGET_US * 2
+        if (range_config_timeout_us > 1100000) { return false }
         range_config_timeout_us = Math.floor(range_config_timeout_us/2)
         let macro_period_us = calcMacroPeriod(readReg(RANGE_CONFIG__VCSEL_PERIOD_A))
         let phasecal_timeout_mclks = timeoutMicrosecondsToMclks(1000, macro_period_us)
         if (phasecal_timeout_mclks > 0xFF) { phasecal_timeout_mclks = 0xFF }
         writeReg(PHASECAL_CONFIG__TIMEOUT_MACROP, phasecal_timeout_mclks)
         writeReg16Bit(MM_CONFIG__TIMEOUT_MACROP_A, encodeTimeout(
-            timeoutMicrosecondsToMclks(1, macro_period_us)));
+            timeoutMicrosecondsToMclks(1, macro_period_us)))
         writeReg16Bit(RANGE_CONFIG__TIMEOUT_MACROP_A, encodeTimeout(
             timeoutMicrosecondsToMclks(range_config_timeout_us, macro_period_us)))
         macro_period_us = calcMacroPeriod(readReg(RANGE_CONFIG__VCSEL_PERIOD_B))
@@ -199,10 +199,10 @@ namespace VL53L1X {
             setupManualCalibration()
             calibrated = true
         }
-        updateDSS();
+        updateDSS()
         let range = results.final_crosstalk_corrected_range_mm_sd0
         ranging_data.range_mm = Math.floor((range * 2011 + 0x0400) / 0x0800)
-        writeReg(SYSTEM__INTERRUPT_CLEAR, 0x01) // sys_interrupt_clear_range
+        writeReg(SYSTEM__INTERRUPT_CLEAR, 0x01)
         return ranging_data.range_mm
     }
 
@@ -211,8 +211,8 @@ namespace VL53L1X {
      */
     //% blockId="VL53L1X_DISTANCE" block="distance"
     export function readSingle(): number {
-        writeReg(SYSTEM__INTERRUPT_CLEAR, 0x01) // sys_interrupt_clear_range
-        writeReg(SYSTEM__MODE_START, 0x10) // mode_range__single_shot
+        writeReg(SYSTEM__INTERRUPT_CLEAR, 0x01)
+        writeReg(SYSTEM__MODE_START, 0x10)
         return read()
     }
 
@@ -230,9 +230,9 @@ namespace VL53L1X {
         saved_vhv_timeout = readReg(VHV_CONFIG__TIMEOUT_MACROP_LOOP_BOUND)
         writeReg(VHV_CONFIG__INIT, saved_vhv_init & 0x7F)
         writeReg(VHV_CONFIG__TIMEOUT_MACROP_LOOP_BOUND,
-            (saved_vhv_timeout & 0x03) + (3 << 2)) // tuning parm default (LOWPOWERAUTO_VHV_LOOP_BOUND_DEFAULT)
+            (saved_vhv_timeout & 0x03) + (3 << 2))
         writeReg(PHASECAL_CONFIG__OVERRIDE, 0x01)
-        writeReg(CAL_CONFIG__VCSEL_START, readReg(PHASECAL_RESULT__VCSEL_START));
+        writeReg(CAL_CONFIG__VCSEL_START, readReg(PHASECAL_RESULT__VCSEL_START))
     }
 
     function readResults(): void {
@@ -259,10 +259,10 @@ namespace VL53L1X {
                 let requiredSpads = Math.floor((TargetRate << 16) / totalRatePerSpad)
                 if (requiredSpads > 0xFFFF || requiredSpads<0) { requiredSpads = 0xFFFF }
                 writeReg16Bit(DSS_CONFIG__MANUAL_EFFECTIVE_SPADS_SELECT, requiredSpads)
-                return;
+                return
             }
         }
-        writeReg16Bit(DSS_CONFIG__MANUAL_EFFECTIVE_SPADS_SELECT, 0x8000);
+        writeReg16Bit(DSS_CONFIG__MANUAL_EFFECTIVE_SPADS_SELECT, 0x8000)
     }
 
     function writeReg(reg: number, d: number): void {
@@ -287,19 +287,19 @@ namespace VL53L1X {
     function readReg(reg: number): number {
         pins.i2cWriteNumber(i2cAddr, reg, NumberFormat.UInt16BE, false)
         let d = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8BE, false)
-        return d;
+        return d
     }
 
     function readReg16Bit(reg: number): number {
         pins.i2cWriteNumber(i2cAddr, reg, NumberFormat.UInt16BE, false)
         let d = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt16BE, false)
-        return d;
+        return d
     }
 
     function readReg32Bit(reg: number): number {
         pins.i2cWriteNumber(i2cAddr, reg, NumberFormat.UInt16BE, false)
         let d = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt32BE, false)
-        return d;
+        return d
     }
 
     function encodeTimeout(timeout_mclks: number): number {
